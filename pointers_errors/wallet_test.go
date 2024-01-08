ec56910405db371579822c8eb7a3584582189cd9
David Dias <daviddiasnogueira@gmail.com>
2024-01-07 23:43:41 -0500

    Added some error handling

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -1,22 +1,10 @@
 package ptrs
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
-
-func TestWallet(t *testing.T) {
-
-	wallet := Wallet{}
-
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
-}
 
 type Bitcoin int
 
@@ -32,10 +20,60 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+	w.balance -= amount
+	return nil
+}
+
 type Stringer interface {
 	String() string
 }
 
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
+}
+
+func TestWallet(t *testing.T) {
+
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Errorf("Wanted an error but didn't get one")
+		}
+	}
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		assertError(t, err)
+	})
 }
