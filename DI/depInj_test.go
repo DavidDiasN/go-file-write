564a2613e738339472801a9dc5c08c28b462221d
David Dias <daviddiasnogueira@gmail.com>
2024-01-09 02:51:16 -0500

    just learning dependecy injection

---
+++ DI/depInj_test.go
@@ -0,0 +1,31 @@
+package depInj
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+
+}
+
