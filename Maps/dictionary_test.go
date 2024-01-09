c41e9553fc07415a8fd51d821af96daf4f74273c
David Dias <daviddiasnogueira@gmail.com>
2024-01-09 00:31:17 -0500

    maps stuff

---
+++ Maps/dictionary_test.go
@@ -0,0 +1,41 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unkown word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}
