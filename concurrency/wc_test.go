323289f86cb5c52695d646c1d0a9fe6d973b0bd3
David Dias <daviddiasnogueira@gmail.com>
2024-01-10 23:58:20 -0500

    Concurrency

---
+++ concurrency/wc_test.go
@@ -0,0 +1,52 @@
+package concurrency
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// func mockWebsiteChecker(url string) bool {
+// 	if url == "waat://furhurterwe.geds" {
+// 		return false
+// 	}
+// 	return true
+// }
+
+// func TestCheckWebsites(t *testing.T) {
+// 	websites := []string{
+// 		"http://google.com",
+// 		"http://blog.gypsydave5.com",
+// 		"waat://furhurterwe.geds",
+// 	}
+
+// 	want := map[string]bool{
+
+// 		"http://google.com":          true,
+// 		"http://blog.gypsydave5.com": true,
+// 		"waat://furhurterwe.geds":    false,
+// 	}
+
+// 	got := Checkwebsites(mockWebsiteChecker, websites)
+
+// 	if !reflect.DeepEqual(want, got) {
+// 		t.Fatalf("wanted %v, got %v", want, got)
+// 	}
+// }
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(200 * time.Millisecond)
+	fmt.Println("we are here")
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Checkwebsites(slowStubWebsiteChecker, urls)
+	}
+}
