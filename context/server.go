c3678b341efc21527478e50d156a4ad7ddb0fb2b
David Dias <daviddiasnogueira@gmail.com>
2024-01-15 02:29:43 -0500

    halfway through context

---
+++ context/server.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+	t         *testing.T
+}
+
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func (s *StubStore) Cancel() {
+
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
